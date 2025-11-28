17b16f16fa015ec5499fcaf47d2b71fd886c246b
Julian D <judadi1994@gmail.com>
2025-11-28 17:59:29 +0100

    Refactor project to remove legacy code and streamline dependencies
    - Deleted legacy components and pages related to the old version of the project, including theme, home pages, and skeletons.
    - Updated `package.json` to remove unused dependencies and ensure a cleaner setup.
    - Simplified `vite.config.ts` by removing unnecessary optimizations and configurations.
    - Adjusted CSS to remove legacy styles, enhancing maintainability.

--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,9 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  optimizeDeps: {
-    include: ['@emotion/react', '@emotion/styled', '@mui/material/Tooltip'],
-  },
   plugins: [
     eslintPlugin(),
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
-    }),
+    react(),
   ],
   build: {
     outDir: 'build',
